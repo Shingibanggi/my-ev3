e221dbebd81f78358bba534dccc1d311ca7e13d7
Jihyun Shin <esther5874@naver.com>
2017-11-01 12:32:43 +0000

    test

--- test1.cpp
+++ test1.cpp
@@ -94,181 +94,253 @@ public:
     {
         m_speed = val;    
     }
+    
 public:
-    void example_code();
     void test_code();
+    void move_right(int n);
+    void move_left(int n);
+    void move_down(int n);
+    void move_up(int n);
+    void open();
+    void close();
+    void take_object(int n);
+    void put_object();
+    void example_code();
 };
 
 
-
-
-// void Crain::example_code()
-// { //This function is for example, you should develop your own logics
-//     while(get_escape() == false)
-//     {
-//         set_down(ev3dev::button::down.pressed());
-//         set_up(ev3dev::button::up.pressed());
-//         set_right(ev3dev::button::right.pressed());
-//         set_left(ev3dev::button::left.pressed());
-//         set_escape(ev3dev::button::back.pressed());
-//         set_enter(ev3dev::button::enter.pressed());
+void Crain::example_code()
+{ //This function is for example, you should develop your own logics
+    while(get_escape() == false)
+    {
+        set_down(ev3dev::button::down.pressed());
+        set_up(ev3dev::button::up.pressed());
+        set_right(ev3dev::button::right.pressed());
+        set_left(ev3dev::button::left.pressed());
+        set_escape(ev3dev::button::back.pressed());
+        set_enter(ev3dev::button::enter.pressed());
         
-//         if(get_up())
-//         {   
-//                 a.set_speed_sp(-1*get_speed());
-//                 a.run_forever();
-//         }   
-//         if(get_down())
-//         {
-//                 a.set_speed_sp(get_speed());
-//                 a.run_forever();
-//         }
-//         if(get_left())
-//         {
-//               b.set_speed_sp(-1*get_speed());
-//               b.run_forever();
-//         }
-//         if(get_right())
-//         {
-//               b.set_speed_sp(get_speed());
-//               b.run_forever();
-//         }
+        if(get_up())
+        {   
+                a.set_speed_sp(-1*get_speed());
+                a.run_forever();
+        }   
+        if(get_down())
+        {
+                a.set_speed_sp(get_speed());
+                a.run_forever();
+        }
+        if(get_left())
+        {
+               b.set_speed_sp(get_speed());
+               b.run_forever();
+        }
+        if(get_right())
+        {
+               b.set_speed_sp(-1* get_speed());
+               b.run_forever();
+        }
        
        
-//         if(!(get_up() | get_down() | get_right() | get_left() | get_enter()))
-//         {
-//             a.set_speed_sp(0);
-//             a.run_forever();
-//             b.set_speed_sp(0);
-//             b.run_forever();
-//         }
-//     }
+        if(!(get_up() | get_down() | get_right() | get_left() | get_enter()))
+        {
+            a.set_speed_sp(0);
+            a.run_forever();
+            b.set_speed_sp(0);
+            b.run_forever();
+        }
+    }
 
-//     a.stop();
-//     b.stop();
+    a.stop();
+    b.stop();
+}
+   
+
+void Crain::move_right(int n)       //end 까지 550?
+{
+    //set_right(true);
+    b.set_speed_sp(get_speed());
+    
+    while(b.position_sp() < n ){
+        b.run_forever();}
+    //b.set_stop_action("hold");
+    b.stop();
+}
+
+void Crain::move_left(int n)
+{
+    //set_left(true);
+    b.set_speed_sp(-1*get_speed());
+            
+    while(b.position_sp() > n ){
+        b.run_forever();}
+    //b.set_stop_action("hold");
+    b.stop();
+}
+
+void Crain::move_down(int n)//100
+{
+    //set_down(true);
+    a.set_speed_sp(get_speed());
+    while(a.position() < n){ 
+        a.run_forever();}
+    //a.set_stop_action("hold");
+    a.stop();
+}
+
+void Crain::move_up(int n) //0
+{
+    //set_up(true);
+    a.set_speed_sp(-1*get_speed());
+    while(a.position() > n){ 
+        a.run_forever();}
+    //a.set_stop_action("hold");
+    a.stop();
+}
+
+void Crain::open()
+{
+    //c.set_polarity("normal");
+    c.set_speed_sp(get_speed());        
+    while(c.duty_cycle() < 20)
+        c.run_forever();
+        
+    c.set_stop_action("hold");       
+    c.stop();
+    
+     //c.set_duty_cycle_sp(20);
+    //c.run_direct()
+    
+    // set_duty_cycle_sp(int v) or set_time_sp(int v)
+    // c.set_command("");       //time or duty cycle
+    // c.set_polarity("normal or inversed");
+}
+
+void Crain::close()
+{
+    c.set_speed_sp(-1*get_speed());
+    while(c.duty_cycle() > 20)
+        c.run_forever();
+    c.set_stop_action("hold");
+    c.stop();
+    
+}
+
+
+void Crain::take_object(int n)          //n=0이면 open
+{
+    if(n==0)
+        open();
+    
+    move_down(100);
+    
+    close();
+    
+    move_up(0);
+}
+
+
+
+void Crain::put_object()
+{
+    move_down(100);
+    
+    open();
+    
+    move_up(0);
+}
+
+
+// crain.move_neck(200, 0);
+     
+//         crain.move_hand(200, 0);
+  
+//         crain.move_neck(200, 1);
+  
+//         for(int i = 0; i < 600; i+=10)
+//         {
+//         crain.move_foot(-1*i, 0);
+//         }
+  
+//         crain.move_hand(100, 1);
+
+
+// void Crain::move_neck(int pos, int flag)
+// {
+//     if (flag == 1)
+//     {
+//         pos *= -1;
+//     }
+//     b.set_position(0);
+//     b.set_position_sp(pos);
+//     b.set_speed_sp(get_speed());
+//     b.run_to_abs_pos();
 // }
+
 
 
 
 void Crain::test_code()
 {
-        float n;
-        int v[3];
-        std::string hold = "hold";
-        int i = 0;
-        
-        // scan
-        
-            // set_right(true);
-            // b.set_speed_sp(get_speed());
-            // while(b.position() < /*v[i]*/ 500)
-            // {
-            //      b.reset();
-            //      b.run_forever();
-                
-            //     n = ultrasonic_q.distance_centimeters();  
-                
-            //     if(n > ultrasonic_q.distance_centimeters()){                            // if 물체를 감지하면
-            //          v[i] = b.position_sp();
-            //          i++;
-            //          sound_q.speak("Ah", true);}
-                
-            //     if(b.position() >= 500)            // End 위치에 도달할 때까지
-            //      {
-            //     if(i == 2){
-            //      //b.set_stop_action(hold);
-            //        b.stop;
-            //        b.reset();
-            //         break;
-            //      }
-                
-            //     else{   
-            //         i = 0;
-            //         b.run_to_abs_pos(0);}          // 시작 위치로
-            //     }  
-            // }
+    float n;
+    int v[3] = {100,230,400};
+    int i = 0;
+    
+
+    // scan
+    
+        // set_right(true);
+        // b.set_speed_sp(get_speed());
+        // while(b.position() < /*v[i]*/ 500)
+        // {
+              b.reset();
+        //      b.run_forever();
             
-                
+        //     n = ultrasonic_q.distance_centimeters();  
+            
+        //     if(n > ultrasonic_q.distance_centimeters()){                            // if 물체를 감지하면
+        //          v[i] = b.position_sp();
+        //          i++;
+        //          sound_q.speak("Ah", true);}
+            
+        //     if(b.position() >= 500)            // End 위치에 도달할 때까지
+        //      {
+        //     if(i == 2){
+        //      //b.set_stop_action(hold);
+        //        b.stop;
+        //         break;
+        //      }
+            
+        //     else{   
+        //         i = 0;
+        //         b.run_to_abs_pos(0);}          // 시작 위치로
+        //     }  
+        // }
+        
         
         //a.reset();    // 현재 neck 위치를 0으로
         
-        // // while문으로(i = 2,1,0)
-        // for(int i = 2; i >= 0 ; i--)
-        //{
-        // i번째 위치로
-            // set_left(true);
-            // b.set_speed_sp(-1*get_speed());
+        for(int i = 2; i >= 0 ; i--)
+        {
             
-            // while(b.position_sp() > /*v[i]*/ ){
-            //     b.run_forever();}
-            // //b.set_stop_action(hold);
-            // b.stop();
-        
-        
-        // 목내리기
-            // set_down(true);
-            // a.set_speed_sp(get_speed());
-            // while(a.position() < 100){ 
-            //     a.run_forever();}
-            // a.set_stop_action("hold");
-            // a.stop();
-        
-        // 집게 벌려서 잡기
-            c.set_polarity("normal");
+            move_right(560);
             
-            while(c.duty_cycle() < 20)
-                c.run_forever();
-                
-            c.stop();
-            // c.set_duty_cycle_sp(20);
-            // c.run_direct()
             
-            // set_duty_cycle_sp(int v) or set_time_sp(int v)
-            // c.set_command("");       //time or duty cycle
-            // c.set_polarity("normal or inversed");
-            // c.set_stop_action("hold");
-        
-        // // 목 올리기
-        //     set_up(true);
-        //     a.set_speed_sp(-1*get_speed());
-        //     while(a.position() > 0){ 
-        //         a.run_forever();}
-        //     //a.set_stop_action(hold);
-        //     a.stop();
-             
-        
-        // // End  위치로
-        //     set_right(true);
-        //     b.set_speed_sp(get_speed());
-        //     while(b.position() < 0){
-        //         b.run_forever();}
-        //     b.set_stop_action(hold);
-        //     b.stop();
-        
-        // // 목 내리기
-        //     set_down(true);
-        //     a.set_speed_sp(get_speed());
-        //     a.set_position(?);
-        //     a.run_to_abs_pos(?);    //값지정해주기
-        //     a.set_stop_action("hold");
-        
-        // // 집게 벌려서 내려놓기
-        
-        // // 목 올리기
-        //     set_down(true);
-        //     a.set_speed_sp(-1*get_speed());
-        //     a.set_position(0);
-        //     //a.run_to_abs_pos(0); 
-        //     a.set_stop_action("hold");
-        // }
+            move_left(v[i]);                    // i 번째 위치로
+
+            // if(i==0)                            // 물건 잡기
+            //     take_object(0);                 
+            // else
+            //     take_object(1);
     
+            //move_right(560);                    // End 위치로
     
-        // Finish
-            
-            
-            
-        //}
-    //}
+            //put_object();                       // 물건 놓기
+        }
+        
+        a.stop();
+        b.stop();
+
 }
 
 
